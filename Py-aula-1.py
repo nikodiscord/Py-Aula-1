af3b179f5386ce3376557006f35c4d9ee15e4865
Niko/Dev <46076276+nikodiscord@users.noreply.github.com>
2019-01-01 10:17:20 -0200

    Update Py-aula-1.py

--- Py-aula-1.py
+++ Py-aula-1.py
@@ -1,29 +1,12 @@
 import discord
 
-color = 0xe081e4
-
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("------------ Carregando -------------")
+    print("Carregando")
     print("Servidores: {} Serves".format(str(len(client.servers))))
-    print("------------ Bot Online -------------")
+    print("Bot Online")
 
-@client.event
-async def on_message(message):
-    if not (not message.content.startswith('!fale')and not message.content.startswith('!falar')):
-        if message.author.id == 'ID DO BOT':
-            return
-        try:
-            mensagem = message.content[7:]
-            await client.delete_message(message)
-            await client.send_message(message.channel, mensagem, tts=False)
-            print('Fale on')
-            print(mensagem)
-        except:
-            embed1 = discord.Embed(color=color)
-            embed1.add_field(name="Falar",value='Você Precisa Digitar Algo Para Eu Falar')
-            await client.send_message(message.channel, embed=embed1)
 
 client.run("")

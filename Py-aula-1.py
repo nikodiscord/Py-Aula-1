b56c653e1dbf38d0ad3bff7ae2005a9142094ce2
Niko/Dev <46076276+nikodiscord@users.noreply.github.com>
2019-01-21 10:11:32 -0200

    Update Py-aula-1.py

--- Py-aula-1.py
+++ Py-aula-1.py
@@ -1,12 +1,12 @@
 import discord
 
-client = discord.Client()
+client = discord.Client ()
 
 @client.event
 async def on_ready():
-    print("Carregando")
+    print(client.user.name)
+    print('Logged in as ---->', client.user)
+    print('ID:', client.user.id)
     print("Servidores: {} Serves".format(str(len(client.servers))))
-    print("Bot Online")
 
-
-client.run("")
+    client.run("")
